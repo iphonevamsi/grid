60f239b3d418efb7184abbbcef3c6c70ec85dfcf
Vinay <rmdort@gmail.com>
2020-08-18 19:27:50 +0800

    Added test for onChangecells

--- packages/spreadsheet/cypress/component/spreadsheet.spec.js
+++ packages/spreadsheet/cypress/component/spreadsheet.spec.js
@@ -1,12 +1,33 @@
 import React from "react";
 import { mount } from "cypress-react-unit-test";
-import SpreadSheet from "./../../src/SpreadSheet";
+import SpreadSheet, { defaultSheets } from "./../../src/SpreadSheet";
 
 describe("Spreadsheet", () => {
+  afterEach(() => {
+    cy.wait(1000);
+  });
   it("mounts", () => {
-    mount(<SpreadSheet onChange={cy.stub().as("handleChange")} />);
+    mount(<SpreadSheet onChange={cy.stub().as("handleChange")} />, {
+      alias: "SpreadSheet"
+    });
     cy.get("@handleChange").should(stub => {
       expect(stub).to.have.been.called;
+    });
+  });
+
+  it("will trigger onChangeCells", () => {
+    mount(<SpreadSheet onChangeCells={cy.stub().as("onChangeCells")} />, {
+      alias: "SpreadSheet"
+    });
+    cy.get(".rowsncolumns-grid-container")
+      .dblclick(60, 60)
+      .get("[data-gramm='false']")
+      .typeInSlate("hello")
+      .wait(100)
+      .type("{enter}");
+    cy.get("@onChangeCells").should(stub => {
+      expect(stub).to.have.been.called;
+      expect(stub).to.have.been.calledWith(defaultSheets[0].id);
     });
   });
 });
